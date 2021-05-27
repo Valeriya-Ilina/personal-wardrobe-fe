cf1914ac6df4f77f2e75c92fa5de01ed75fbd712
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-05-26 22:01:55 -0400

    Footer is created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Card, Button } from 'react-bootstrap';
 
 function Footer(props) {
   return(
-    <div>
-      <h1>Footer</h1>
+    <div class='footer'>
+      <Card className="text-center">
+      <Card.Body>
+        <Card.Title>Valeriya Ilina</Card.Title>
+        <a href="https://github.com/Valeriya-Ilina">GitHub</a>
+      </Card.Body>
+      <Card.Footer className="text-muted">June, 2021</Card.Footer>
+      </Card>
     </div>
   )
 }
+
+
 
 export default Footer;
