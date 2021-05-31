19e1a71627cbd61ffbcdf09c89e256e75a54c909
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-05-30 22:45:42 -0400

    render items by category on the Wish List page

--- src/components/ItemsWishList.js
+++ src/components/ItemsWishList.js
@@ -60,18 +60,27 @@ class ItemsWishList extends Component {
       <>
         <div>
           <h1>Wish list items</h1>
-          {/*{
-            wishListItems.map((item, idx) => {
+          {
+            categoriesWithItemsWishList.map((category, idx) => {
               return (
                 <div key={idx}>
-                  <img class='item' src={item.imageUrl} alt={item.name} />
-                  <p>${item.price}</p>
-                  <a href={item.itemInStoreUrl}>Go to store</a>
-                  <button onClick={()=>this.editItem(item)}>Edit</button>
+                  <h3>{category.category_name}</h3>
+                  {
+                    category.items.map((item) => {
+                      return (
+                        <>
+                          <img class='item' src={item.imageUrl} alt={item.name} />
+                          <p>${item.price}</p>
+                          <a href={item.itemInStoreUrl}>Go to store</a>
+                          <button onClick={()=>this.editItem(item)}>Edit</button>
+                        </>
+                      )
+                    })
+                  }
                 </div>
               )
             })
-          }*/}
+          }
 
           <Button onClick={this.handleShow}><img src="https://icons-for-free.com/iconfiles/png/512/circle+create+new+plus+sign+icon-1320085936892806512.png" alt="Add new item button" class='add-item-btn'/></Button>
         </div>
