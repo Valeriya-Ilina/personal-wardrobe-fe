aa7d57690e7f80708bdd4e753c972e6505f291cf
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-06-01 20:32:40 -0400

    outfits page shows a list of outfits as a paragraph text

--- src/components/Outfits.js
+++ src/components/Outfits.js
@@ -1,15 +1,56 @@
 import { Component } from 'react'
 
+let baseURL = 'http://127.0.0.1:8000'
+
 class Outfits extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      outfits: []
+    }
+  }
+
+  componentDidMount() {
+    this.getOutfits()
+  }
+
+  getOutfits = async () => {
+    const url = baseURL + '/api/v1/outfits/'
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        credentials: "include"
+      })
+      // convert response to json
+      const jsonResponse = await response.json()
+
+      if (response.status === 200) {
+        this.setState({
+          outfits: jsonResponse.data
+        })
+      }
+    }
+    catch (err) {
+      console.log('Error => ', err)
+    }
   }
 
   render() {
+    console.log(this.state)
     return(
-        <div>
-          <h1>Outfits</h1>
+      <div>
+        <h1>Outfits</h1>
+        <div className="outfits-container">
+          {
+            this.state.outfits.map(outfit => {
+              return (
+                <p>{outfit.name}</p>
+              )
+            })
+          }
         </div>
+      </div>
     )
   }
 }
