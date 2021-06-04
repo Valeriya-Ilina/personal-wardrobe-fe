f56c934f0415fdb3a9a0bf3d53f75797a74f71f2
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-06-04 17:11:35 -0400

    updated links and styles on the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import { Component } from 'react'
+import { Button } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import './../App.css'
 
 class Home extends Component {
   constructor(props) {
@@ -7,11 +10,24 @@ class Home extends Component {
 
   render() {
     return(
-      <>
-        <div>
-          <h1>Home</h1>
+      <div class='container'>
+        <div class="home-image">
+          <img src='./images/main_page7.jpeg' class="backgroundImg7" />
+          <Button as={Link} to='/wishlist' class="btn">Wish List</Button>
         </div>
-      </>
+        <div class='container'>
+          <div class='row'>
+            <span class="home-image">
+              <img src='./images/main_page2.jpeg' class="backgroundImg2" />
+              <Button as={Link} to='/wishlist' class="btn">Your Wardrobe</Button>
+            </span>
+            <span class="home-image">
+              <img src='./images/main_page4.jpeg' class="backgroundImg4" />
+              <Button as={Link} to='/wishlist' class="btn">Outfits</Button>
+            </span>
+          </div>
+        </div>
+      </div>
     )
   }
 }
